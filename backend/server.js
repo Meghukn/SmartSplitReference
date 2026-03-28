f128eb91088436f54d15cc17f7d56bfe99ebfd5b
Meghana <meghukn16@gmail.com>
2026-03-28 22:09:53 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,13 @@ const app = express();
 // connect database
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_URL
+      : "http://localhost:3000",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
